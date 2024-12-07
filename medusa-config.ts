c6127505a0c5e20a80d1e2fd1f6daba9e5d00816
Harry K <harry.c.knowles@gmail.com>
2024-12-07 11:22:32 +0100

    add sslmode to postgres url

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,7 +6,8 @@ module.exports = defineConfig({
   projectConfig: {
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
+    redisUrl: process.env.REDIS_URL + '?sslmode=require' ,
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
