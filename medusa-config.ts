20a2fd3b5147ed00fa02568b639caba112e80171
Harry K <harry.c.knowles@gmail.com>
2024-12-07 11:34:13 +0100

    set ssl mode for postgres

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,8 +6,10 @@ module.exports = defineConfig({
   projectConfig: {
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL + '?sslmode=require' ,
+    redisUrl: process.env.REDIS_URL ,
     backendUrl: process.env.MEDUSA_BACKEND_URL,
+    databaseDriverOptions: process.env.NODE_ENV !== "development"
+        ? { ssl: { rejectUnauthorized: false } } : {},
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
